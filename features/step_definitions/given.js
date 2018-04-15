c2d68b92dcc829722ece99e26d147f6ebee318d8
Alex Schouls <alex_schouls@live.com>
2018-04-15 21:37:04 +0200

    Starting setup

---
+++ features/step_definitions/given.js
@@ -0,0 +1,21 @@
+import { defineSupportCode } from 'cucumber';
+
+var config = require('../generic/config.js');
+var url;
+
+defineSupportCode(( { Given }) => {
+    
+    // *
+    // General functions
+
+    // Visiting an url
+    Given(/^I have visited '([^']*)'$/, function (page) {   
+        browser.deleteCookie();
+        browser.setViewportSize({
+            width: config.screenSize.width,
+            height: config.screenSize.height
+        });
+        url = browser.options.baseUrl + page;
+        browser.url(url);
+    });
+});
